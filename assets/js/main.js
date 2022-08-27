4b8d479a75f48ed5ef7f24bb1fdc0cb6b79f827c
Emanuelle30 <emanuelle.cavalcante4@gmail.com>
2022-08-27 20:45:31 -0300

    Inserindo alert e limpeza do forms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,27 @@
+import { subscribeToHellfireClub } from "./firebase/hellfire-clube.js";
 
-import {subscribeToHellfireClub} from './firebase/hellfire-clube.js'
+const txtName = document.getElementById("txtName");
+const txtEmail = document.getElementById("txtEmail");
+const txtLevel = document.getElementById("txtLevel");
+const txtCharacter = document.getElementById("txtCharacter");
 
-const txtName = document.getElementById('txtName')
-const txtEmail = document.getElementById('txtEmail')
-const txtLevel = document.getElementById('txtLevel')
-const txtCharacter = document.getElementById('txtCharacter')
+const btnSubscribe = document.getElementById("btnSubscribe");
 
-const btnSubscribe = document.getElementById('btnSubscribe')
+btnSubscribe.addEventListener("click", async () => {
+  const subscription = {
+    name: txtName.value,
+    email: txtEmail.value,
+    level: txtLevel.value,
+    character: txtCharacter.value,
+  };
 
-btnSubscribe.addEventListener('click', async () => {
-    const subscription = {
-        name: txtName.value,
-        email:txtEmail.value,
-        level:txtLevel.value,
-        character: txtCharacter.value
-    }
+  const subscriptionId = await subscribeToHellfireClub(subscription);
+  
+  txtName.value = ' '
+  txtEmail.value = ' '
+  txtLevel.value = ' '
+  txtCharacter.value = ' '
 
-   const subscriptionId = await subscribeToHellfireClub(subscription)
-   console.log(`Inscrito com sucesso: ${subscriptionId}`)
-})
+  alert(`Inscrito com sucesso: ${subscriptionId}`);
 
+});
